18828b608bbdb46245d66f94a417f01a9cf08698
Roman Lomov <romario.lomov@gmail.com>
2024-07-13 18:34:34 +0600

    Implement Util class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/pp_1_db";
+    private final static String USERNAME = "bestuser";
+    private final static String PASSWORD = "bestuser";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
